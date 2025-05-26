48886e8f5046bdda90684f3f7ec54d47df3f24f9
Vinicius Karabolsak <viniciuskarabolsak@hotmail.com>
2025-05-26 16:40:10 -0300

    novo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,11 @@
-import  { useState } from "react";
+import { useState } from "react";
 import logo from "../assets/restaurante.png";
 import './stylesidebar.css';
 
 export default function Sidebar({ onSelect }: { onSelect: (value: string) => void }) {
-  
-  const menus = ['Dashboard', 'Produtos', 'Clientes', 'Relatórios'];
-
-  // Estado para controlar o menu ativo - inicializado com 'Dashboard'
+  const menus = ['Dashboard', 'Relatórios', 'Produtos', 'Clientes', 'Novos itens'];
   const [activeMenu, setActiveMenu] = useState('Dashboard');
 
-  // Função para selecionar menu
   function handleSelect(item: string) {
     setActiveMenu(item);
     onSelect(item);
@@ -18,30 +14,40 @@ export default function Sidebar({ onSelect }: { onSelect: (value: string) => voi
   return (
     <div className="sideBar">
       <div className="teste1">
-        <img 
-          src={logo} 
-          alt="imagem-Logo-loja" 
+        <img
+          src={logo}
+          alt="imagem-Logo-loja"
           className="logo-loja"
         />
-      
         <h1 className="h1Sidebar">Esfirraria do Zé</h1>
       </div>
-      <ul>
-        {menus.map((item) => (
-          <li
-            key={item}
-            onClick={() => handleSelect(item)}
-            className={`
-              cursor-pointer 
-              p-2 rounded 
-              hover:bg-gray-700 
-              ${activeMenu === item ? 'bg-gray-700 font-bold' : ''}
-            `}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
+
+      <div className="menu-container">
+        <div
+          className="borda-animada"
+          style={{
+            top: `${menus.indexOf(activeMenu) * 44}px`,
+            height: '44px',
+          }}
+        />
+
+        <ul>
+          {menus.map((item) => (
+            <li
+              key={item}
+              onClick={() => handleSelect(item)}
+              className={`
+                cursor-pointer 
+                p-3 rounded 
+                hover:bg-gray-700 
+                ${activeMenu === item ? 'bg-gray-700 font-bold' : ''}
+              `}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
